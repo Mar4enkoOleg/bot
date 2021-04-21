5b37238881de17f4c413480af71c2c16d55c741e
Mar4enkoOleg <rosstofor@gmail.com>
2021-04-21 22:51:52 +0300

    update .gitignore

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const config_1 = require("./config");
-const app = express_1.default();
-app.listen(config_1.server.port, () => console.log(`Server start on ${config_1.server.port} port`));
--- dist/routes/users.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const users_controller_1 = require("../controllers/users.controller");
-const router = express_1.Router();
-router.get('/', users_controller_1.getAllUsers);
-router.get('/:id', users_controller_1.getUser);
-router.put('/:id', users_controller_1.updateUser);
-router.delete('/:id', users_controller_1.deleteUser);
