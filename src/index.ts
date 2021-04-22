611a142ba1e4621dac635eccb6a658c4f1ff1292
Mar4enkoOleg <rosstofor@gmail.com>
2021-04-22 04:35:16 +0300

    update port import

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
 import express from 'express'
-// import { server } from './config'
+import { server } from './config'
 import router from './routes/users'
 
 const app = express()
 
-const port = process.env.PORT || 3000
-
 app.use('/users', router)
 
-app.listen(port, () => console.log(`Server start on ${port} port`))
+app.listen(server.port, () => console.log(`Server start on ${server.port} port`))
