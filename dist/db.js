a44dbdd7dc0d02d37c24359b415d32cea3e45dee
Mar4enkoOleg <rosstofor@gmail.com>
2021-04-24 05:10:42 +0300

    test heroku postgres

--- dist/db.js
+++ dist/db.js
@@ -2,4 +2,4 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
 const config_1 = require("./config");
-exports.default = new sequelize_1.Sequelize(config_1.db.full_address);
+exports.default = new sequelize_1.Sequelize(config_1.db.full_address, { ssl: true });
