622facd677b31467e6ac913ad6edda4f1b49e311
kaktus312 <22836793+kaktus312@users.noreply.github.com>
2021-05-09 20:16:39 +0300

    Add dist folder to .gitignore and delete it from repo

--- dist/entities/info/infoRouter.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const infoController_1 = require("./infoController");
-const router = express_1.Router();
-router.get('/bot', infoController_1.getBotInfo);
-router.get('/cafedra', infoController_1.getCafedraInfo);
-router.put('/cafedra', infoController_1.updateCafedraInfo);
-exports.default = router;
--- dist/entities/user/updateAdminRouter.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const updateAdmin_controller_1 = require("./updateAdmin.controller");
-const router = express_1.Router();
-router.get('/getAdmins', updateAdmin_controller_1.getAllAdmins);
-router.put('/changeAdminToUser/:username', updateAdmin_controller_1.changeAdminToUser);
-router.put('/addAdmin/:username', updateAdmin_controller_1.changeUserToAdmin);
-exports.default = router;
