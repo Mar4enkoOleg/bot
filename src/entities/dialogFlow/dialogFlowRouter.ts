284aebc968f1f64e63e8f6aff9be79f47f06ddf7
kaktus312 <22836793+kaktus312@users.noreply.github.com>
2021-05-12 08:50:32 +0300

    Create file structure for DialogFlow

---
+++ src/entities/dialogFlow/dialogFlowRouter.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+
+import tryCatchWrapper from '../../helpers/tryCatchWrapper';
+
+
+const df = Router();
+
+// df.post('/dialogFlow', tryCatchWrapper(getBotInfo));
+
+export default  df;
